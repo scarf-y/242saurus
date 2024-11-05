be33c26274dff1034eb41b7beba7f44e8819af31
scarf-y <158463988+scarf-y@users.noreply.github.com>
2024-11-05 12:20:59 +0700

    changed navbar logo

--- Docusaurus/docusaurus.config.ts
+++ Docusaurus/docusaurus.config.ts
@@ -70,7 +70,7 @@ const config: Config = {
       title: 'My Site',
       logo: {
         alt: 'My Site Logo',
-        src: 'img/logo.svg',
+        src: 'img/capybara-logo.png',
       },
       items: [
         {
